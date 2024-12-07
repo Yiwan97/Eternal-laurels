09e79a6145f3d56470aca8ad0c9887ecb058807b
Godoy Villalba, Ivan <111784352+Yiwan97@users.noreply.github.com>
2024-12-07 14:18:45 +0000

    new file:   eternal-laurels/public/Titulo.png 	new file:   eternal-laurels/src/api/batalla/mover.js

---
+++ eternal-laurels/src/api/batalla/mover.js
@@ -0,0 +1,17 @@
+function Mover(unidad, jugador, posicion) {
+  if(unidad.jugador == jugador) {
+    if(unidad.posicion == null) {
+      if(posicion[1] < 2) {
+        return Movimientos;
+      } else {
+        return MessageEvent('La unidad no se puede mover a esa posicion en este turno')
+      }
+    } else {
+      if(posicion){
+        
+      }
+    }
+  } else {
+    return MessageEvent('Solo puedes mover tus unidades')
+  }
+}
