90ca0f2079ee9c2208c1862949c592d5e94cf256
Godoy Villalba, Ivan <111784352+Yiwan97@users.noreply.github.com>
2025-03-01 13:21:19 -0300

    Actualizar foro

---
+++ eternal-laurels/src/componentes/foro/busqueda.jsx
@@ -0,0 +1,12 @@
+import busqueda from './supabase/busqueda'
+function Buscar() {
+  return(
+    <div>
+      <search><form action='busqueda'>
+<input tupe='search>
+<button>Buscar</button>
+</form>
+</search>
+);
+
+export default Buscar;
