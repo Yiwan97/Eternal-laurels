a2ad9540c7de905a5977925188f7e4ea627e58ff
Godoy Villalba, Ivan <111784352+Yiwan97@users.noreply.github.com>
2025-03-08 19:48:03 -0300

    Actualizar lateral.jsx

--- eternal-laurels/src/componentes/foro/lateral.jsx
+++ eternal-laurels/src/componentes/foro/lateral.jsx
@@ -1,21 +1,7 @@
 function Lateral () {
   return (
-    <div class='h-[80%] w-[30%] bg-[#0000ff]'>
-      <div class='absolute text-center left-[72%] top-[15%] h-[22%] w-[26%] bg-[#ff0000]'>
-        <a href='/foro/editor'><p>Editor</p></a>
-        <a href='#'><p>Anteultima</p></a>
-        <a href='#'><p>Penultima</p></a>
-        <a href='#'><p>Antepenultima</p></a>
-        <a href='#'><p>Quinta</p></a>
-      </div>
-      <div class='absolute left-[72%] top-[40%] h-[22%] w-[26%] text-center bg-[#ff00ff]'>
-        <a href='#'><p></p></a>
-        <a href='#'><p></p></a>
-        <a href='#'><p></p></a>
-        <a href='#'><p></p></a>
-        <a href='#'><p></p></a>
-      </div>
-    </div>
+    <div>
+      
   );
 }
 
